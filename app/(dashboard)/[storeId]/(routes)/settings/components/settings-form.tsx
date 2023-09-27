2c70b66b85b46c0297359d6f8ee720fb6b7729f1
EugeneLKL <eugenelkl02@gmail.com>
2023-09-28 01:32:33 +0800

    Create and edit bill boards

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -56,7 +56,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
       router.refresh();
       toast.success("Store updated.");
     } catch (error) {
-      toast.error("Something went wrong.");
+      toast.error("Failed to update store.");
     } finally {
       setLoading(false);
     }
